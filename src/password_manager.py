b1a0fe5ae0dfb726ced4e6ba880c74d03c7d8ebb
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-12 19:29:32 +1100

    Add buttons

--- src/password_manager.py
+++ src/password_manager.py
@@ -1,9 +1,22 @@
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        
+        super().__init__(master)
+        
+        self.create_widgets()
+        print("I've been made")
+    
+    def create_widgets(self):
+        # heading
+        self.label = tk.Label(root, text="Add entry")
+        self.label.pack()
+        # submit button
+        self.my_button = tk.Button(root, text="Submit")
+        self.my_button.pack()
 
-label = Label(root, text = "Add entry")
 
-label.pack()
-
-root.mainloop()
+root = tk.Tk()
+app = Application(root)
+app.mainloop()
