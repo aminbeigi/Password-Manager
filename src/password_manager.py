677304e705749762ee6d7e81322cafef8221b8d2
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-12 21:05:05 +1100

    Add submit method

--- src/password_manager.py
+++ src/password_manager.py
@@ -1,20 +1,25 @@
 import tkinter as tk
 
+# generate the user interface
 class Application(tk.Frame):
-    def __init__(self, master=None):
-        
+    def __init__(self, master):
         super().__init__(master)
-        
+        self.pack()
         self.create_widgets()
-        print("I've been made")
-    
+
+    def submit(self):
+        print("The submit button has been pressed.")
+
     def create_widgets(self):
         # heading
-        self.label = tk.Label(root, text="Add entry")
-        self.label.pack()
+        self.heading = tk.Label(root, text="Add entry")
+        self.heading.pack()
+        # input fields
+        self.email = tk.Label(root, text="Email: ")
+        self.email.pack()
         # submit button
-        self.my_button = tk.Button(root, text="Submit")
-        self.my_button.pack()
+        self.submit_buttom = tk.Button(root, text="Submit", command=self.submit)
+        self.submit_buttom.pack()
 
 
 root = tk.Tk()
