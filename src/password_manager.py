c75bc862c2909e4cd9c6c76f1625fce65f9862b8
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-12 21:50:12 +1100

    Add all buttons

--- src/password_manager.py
+++ src/password_manager.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 
+# global variables
+root = tk.Tk()
+
 # generate the user interface
 class Application(tk.Frame):
     def __init__(self, master):
@@ -7,24 +10,40 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def submit(self):
-        print(self.input_email.get())
+        title = self.title_entry.get()
+        email = self.email_entry.get()
+        password = self.password_entry.get()
+        print(title, email, password)
 
     def create_widgets(self):
         # heading
-        self.label_heading = tk.Label(root, text="Add entry")
+        self.label_heading = tk.Label(root, text="Add entry", font="arial 24")
         self.label_heading.grid(row=0, column=0)
-        # input fields
-        self.label_email = tk.Label(root, text="Email: ")
-        self.label_email.grid(row=1, column=0)
-        self.input_email = tk.Entry(root)
-        self.input_email.grid(row=1, column=1)
+        ### input fields ###
+        self.title_label = tk.Label(root, text="Title: ", font="arial 16")
+        self.title_label.grid(row=1, column=0)
+        self.title_entry = tk.Entry(root)
+        self.title_entry.grid(row=1, column=1)
 
+        self.email_label = tk.Label(root, text="Email: ", font="arial 16")
+        self.email_label.grid(row=2, column=0)
+        self.email_entry = tk.Entry(root)
+        self.email_entry.grid(row=2, column=1)
+
+        self.password_label = tk.Label(root, text="Password: ", font="arial 16")
+        self.password_label.grid(row=3, column=0)
+        self.password_entry = tk.Entry(root, show='*')
+        self.password_entry.grid(row=3, column=1)       
 
         # submit button
-        self.submit_buttom = tk.Button(root, text="Submit", command=self.submit)
+        self.submit_buttom = tk.Button(root, text="Submit", font="arial 16", command=self.submit)
         self.submit_buttom.grid(row=99, column=99)
 
-
-root = tk.Tk()
-app = Application(root)
-app.mainloop()
+# entry to program
+def main():
+    root.title("Password Manager")
+    app = Application(root)
+    app.mainloop()
+ 
+if __name__ == '__main__':
+    main()
