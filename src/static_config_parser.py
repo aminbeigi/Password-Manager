3bb9dc214269b4d0f4c877dafcc5cf483eea7a2b
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-24 13:02:52 +1100

    Fix typo

--- src/static_config_parser.py
+++ src/static_config_parser.py
@@ -2,7 +2,7 @@ import configparser
 """A config parser that is used by the modules main, database and encryption."""
 
 class StaticConfigParser():
-    CONFIG_FILE_PATH = 'config.ini'
+    CONFIG_FILE_PATH = './config/config.ini'
     config = configparser.ConfigParser()
     config.read(CONFIG_FILE_PATH)
     
