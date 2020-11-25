436db86078cb38bb3255f47a366aa4c16bd4dea1
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-25 11:49:40 +1100

    Add test

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -6,9 +6,23 @@ class TestStringMethods(unittest.TestCase):
         self.encryption = Encryption()
         self.key = self.encryption.generate_key()
 
-    def test_numbers(self):
+    def test_ASCII(self):
         plain_text = 'fmZG18Cto'
         self.assertEqual(self.encryption.decrypt(self.encryption.encrypt(plain_text, self.key).decode(), self.key), plain_text)
+        
+        plain_text = ':oGb[ FlHX5#a^psr+2y'
+        self.assertEqual(self.encryption.decrypt(self.encryption.encrypt(plain_text, self.key).decode(), self.key), plain_text)
+
+    def test_unicode(self):
+        plain_text = 'العربية'
+        self.assertEqual(self.encryption.decrypt(self.encryption.encrypt(plain_text, self.key).decode(), self.key), plain_text)
+
+        plain_text = '汉语'
+        self.assertEqual(self.encryption.decrypt(self.encryption.encrypt(plain_text, self.key).decode(), self.key), plain_text)
+
+        plain_text = '🦆🚀🔥🦕🧹🦈'
+        self.assertEqual(self.encryption.decrypt(self.encryption.encrypt(plain_text, self.key).decode(), self.key), plain_text)
+
 
 if __name__ == '__main__':
     unittest.main()
