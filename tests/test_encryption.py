f09e5bcedaaf44fbcd1eafbc7148f726500bf8d4
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-25 11:21:47 +1100

    Add a test

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -2,9 +2,13 @@ import unittest
 from passwordmanager import Encryption
 
 class TestStringMethods(unittest.TestCase):
-    
+    def setUp(self):
+        self.encryption = Encryption()
+        self.key = self.encryption.generate_key()
+
     def test_numbers(self):
-        self.assertEqual('meme', 'meme')
+        plain_text = 'fmZG18Cto'
+        self.assertEqual(self.encryption.decrypt(self.encryption.encrypt(plain_text, self.key).decode(), self.key), plain_text)
 
 if __name__ == '__main__':
     unittest.main()
