259296632d6045c7a30b6d83d73d407db9f65ff8
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-26 17:07:37 +1100

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='passwordmanager',
-    version='1.0.0',
+    version='1.1.0',
     description='An open source account manager.',
     packages=['passwordmanager'],
     license='MIT',
