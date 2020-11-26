a9dcd09f5bfe309dbeb0b9fd1323000b99021e1b
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-26 14:22:53 +1100

    Add a try-catch block

--- passwordmanager/static_config_parser.py
+++ passwordmanager/static_config_parser.py
@@ -3,9 +3,14 @@ import configparser
 """A static config parser that is used by the modules main, database and encryption."""
 
 class StaticConfigParser():
-    CONFIG_FILE_PATH = './config/config.ini'
+    config_file_path = './config/config1.ini'
     config = configparser.ConfigParser()
-    config.read(CONFIG_FILE_PATH)
+
+    try:
+        with open(config_file_path) as f:
+            config.read(config_file_path)
+    except (OSError, IOError) as e:
+        raise Exception("Couldn't find path to config.ini.") from e
     
     @staticmethod
     def get(parent, child):
